76139d4fe5969adf90cf6045c344c6747859bf68
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-10 21:55:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+ // Find drivers that match the passed-in name
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Find drivers whose names start with the provided letters
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Find drivers with a matching name in the data structure
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
